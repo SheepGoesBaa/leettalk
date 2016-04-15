5ca3491ce2478476be9010d4e887c00423b1c68f
frankyi5 <yi43@purdue.edu>
2016-04-15 15:47:04 -0400

    initial commit

---
+++ src/main/java/leettalk/LeettalkApplication.java
@@ -0,0 +1,12 @@
+package leettalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeettalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeettalkApplication.class, args);
+	}
+}
---
+++ src/main/java/leettalk/ServletInitializer.java
@@ -0,0 +1,13 @@
+package leettalk;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LeettalkApplication.class);
+	}
+
+}
