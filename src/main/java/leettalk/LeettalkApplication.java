177759ed2578d7a7393177c60f5f4d579d64cbfb
frankyi5 <yi43@purdue.edu>
2016-04-26 01:17:58 -0400

    removed debug features

--- src/main/java/leettalk/LeettalkApplication.java
+++ src/main/java/leettalk/LeettalkApplication.java
@@ -11,8 +11,5 @@ public class LeettalkApplication {
 
 	public static void main(String[] args) throws IOException {
 		ConfigurableApplicationContext context = SpringApplication.run(LeettalkApplication.class, args);
-		System.out.println("Hit 'Enter' to terminate");
-		System.in.read();
-		context.close();
 	}
 }
